d39246c046bd03b54513c1218b7cdf4376a870f9
Johan Bos <bosbosjohan@gmail.com>
2019-04-20 18:15:02 +0200

    - Git ignore

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyA8PrcQrX8_NqfUwMRwrBlNrI7hkI2SKWY',
-  authDomain: 'lets-meet-2019.firebaseapp.com',
-  databaseURL: 'https://lets-meet-2019.firebaseio.com',
-  projectId: 'lets-meet-2019',
-  storageBucket: 'lets-meet-2019.appspot.com',
-  messagingSenderId: '20650399097'
-};
