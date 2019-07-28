6e2713f070e49adf064648f92df49a984a8fba7b
Johan Bos <bosbosjohan@gmail.com>
2019-07-28 21:58:05 +0200

    Fixed scroll issue

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -7,108 +7,121 @@ import { AuthenticationService } from 'src/app/services/helpers/authentication.s
 import { Contact } from 'src/app/services/models/contact';
 
 @Component({
-  selector: 'app-contacts',
-  templateUrl: './contacts.page.html',
-  styleUrls: ['./contacts.page.scss'],
+	selector: 'app-contacts',
+	templateUrl: './contacts.page.html',
+	styleUrls: ['./contacts.page.scss']
 })
 export class ContactsPage implements OnInit {
-  authUser: firebase.User = null;
-  contacts: Contact[] = null;
+	authUser: firebase.User = null;
+	contacts: Contact[] = null;
 
-  constructor(private router: Router, private contactService: ContactService, public _sanitizer: DomSanitizer,
-    private authenticationService: AuthenticationService, private alertController: AlertController,
-    public menuController: MenuController) { }
+	searchQuery = '';
 
-  ngOnInit() {
-    this.authUser = this.authenticationService.getUserAuth();
-    this.getContacts();
-  }
+	constructor(
+		private router: Router,
+		private contactService: ContactService,
+		public _sanitizer: DomSanitizer,
+		private authenticationService: AuthenticationService,
+		private alertController: AlertController,
+		public menuController: MenuController
+	) {}
 
-  getContacts() {
-    this.contactService.getContacts(this.authUser.uid).subscribe(
-      (response) => { setTimeout(() => this.contacts = response, 1200); }
-    );
-  }
+	ngOnInit() {
+		this.authUser = this.authenticationService.getUserAuth();
+		this.getContacts();
+	}
 
-  toggleSideNav() {
-    this.menuController.toggle();
-  }
+	getContacts() {
+		this.contactService.getContacts(this.authUser.uid).subscribe(response => {
+			setTimeout(() => (this.contacts = response), 1200);
+		});
+	}
 
-  refreshContacts(event: any) {
-    setTimeout(() => {
-      this.getContacts();
-      event.target.complete();
-    }, 2000);
-  }
+	toggleSideNav() {
+		this.menuController.toggle();
+	}
 
-  async deleteContact(contact: Contact) {
-    const alert = await this.alertController.create({
-      header: 'Delete Contact',
-      message: 'Are you sure you want to delete this contact?',
-      buttons: [
-        {
-          text: 'Yes',
-          handler: () => {
-            this.contactService.deleteContact(contact.id).then((response) => {
-              this.getContacts();
-            });
-          }
-        },
-        {
-          text: 'No'
-        }
-      ]
-    });
+	refreshContacts(event: any) {
+		setTimeout(() => {
+			this.getContacts();
+			event.target.complete();
+		}, 2000);
+	}
 
-    await alert.present();
-  }
+	async deleteContact(contact: Contact) {
+		const alert = await this.alertController.create({
+			header: 'Delete Contact',
+			message: 'Are you sure you want to delete this contact?',
+			buttons: [
+				{
+					text: 'Yes',
+					handler: () => {
+						this.contactService.deleteContact(contact.id).then(response => {
+							this.getContacts();
+						});
+					}
+				},
+				{
+					text: 'No'
+				}
+			]
+		});
 
+		await alert.present();
+	}
 
-  async showPrompt() {
-    const alert = await this.alertController.create({
-      header: 'Add Contact',
-      message: 'Please enter the email of the contact you would like to add.',
-      inputs: [
-        {
-          name: 'contactEmail',
-          type: 'email',
-          id: 'contactEmail',
-          placeholder: 'Email'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          cssClass: 'secondary'
-        }, {
-          text: 'OK',
-          handler: (data) => {
-            let contactEmail = data.contactEmail;
-            this.contactService.addContactByEmail(contactEmail).then(
-              async (response) => {
-                if (response == false) {
-                  alert.dismiss();
+	async showPrompt() {
+		const alert = await this.alertController.create({
+			header: 'Add Contact',
+			message: 'Please enter the email of the contact you would like to add.',
+			inputs: [
+				{
+					name: 'contactEmail',
+					type: 'email',
+					id: 'contactEmail',
+					placeholder: 'Email'
+				}
+			],
+			buttons: [
+				{
+					text: 'Cancel',
+					role: 'cancel',
+					cssClass: 'secondary'
+				},
+				{
+					text: 'OK',
+					handler: data => {
+						let contactEmail = data.contactEmail;
+						this.contactService
+							.addContactByEmail(contactEmail)
+							.then(async response => {
+								if (response === false) {
+									alert.dismiss();
 
-                  const errorAlert = await this.alertController.create({
-                    header: 'Error',
-                    message: 'No user found matching this email!',
-                    buttons: [{ text: 'OK', handler: () => this.getContacts() }]
-                  });
-                  await errorAlert.present();
-                } else {
-                  alert.dismiss();
-                }
-              });
-          }
-        }
-      ]
-    });
+									const errorAlert = await this.alertController.create({
+										header: 'Error',
+										message: 'No user found matching this email!',
+										buttons: [
+											{
+												text: 'OK',
+												handler: () => this.getContacts()
+											}
+										]
+									});
+									await errorAlert.present();
+								} else {
+									alert.dismiss();
+								}
+							});
+					}
+				}
+			]
+		});
 
-    await alert.present();
-  }
+		await alert.present();
+	}
 
-  navigateBack() {
-    this.router.navigate(['home/profile']);
-  }
+	navigateBack() {
+		this.router.navigate(['home/profile']);
+	}
 }
--- src/app/pages/groups/groups.page.ts
+++ src/app/pages/groups/groups.page.ts
@@ -1,83 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { AlertController, MenuController, NavController } from '@ionic/angular';
+import { AlertController, MenuController } from '@ionic/angular';
 import { DataService } from 'src/app/services/data.service';
 import { GroupService } from 'src/app/services/group.service';
 import { AuthenticationService } from 'src/app/services/helpers/authentication.service';
 import { Group } from 'src/app/services/models/group';
 
 @Component({
-  selector: 'app-groups',
-  templateUrl: './groups.page.html',
-  styleUrls: ['./groups.page.scss'],
+	selector: 'app-groups',
+	templateUrl: './groups.page.html',
+	styleUrls: ['./groups.page.scss']
 })
 export class GroupsPage implements OnInit {
-  authUser: firebase.User = null;
-  groups: Group[] = null;
+	authUser: firebase.User = null;
+	groups: Group[] = null;
 
+	searchQuery = '';
 
-  constructor(private router: Router, private groupService: GroupService, public _sanitizer: DomSanitizer, private navController: NavController,
-    private authenticationService: AuthenticationService, private alertController: AlertController, private dataService: DataService,
-    public menuController: MenuController) { }
+	constructor(
+		private router: Router,
+		private groupService: GroupService,
+		public _sanitizer: DomSanitizer,
+		private authenticationService: AuthenticationService,
+		private alertController: AlertController,
+		private dataService: DataService,
+		public menuController: MenuController
+	) {}
 
-  ngOnInit() {
-    this.authUser = this.authenticationService.getUserAuth();
-    this.getGroups();
-  }
+	ngOnInit() {
+		this.authUser = this.authenticationService.getUserAuth();
+		this.getGroups();
+	}
 
-  getGroups() {
-    this.groupService.getGroups(this.authUser.uid).subscribe(
-      (response) => { setTimeout(() => this.groups = response, 1200); }
-    );
-  }
+	getGroups() {
+		this.groupService.getGroups(this.authUser.uid).subscribe(response => {
+			setTimeout(() => (this.groups = response), 1200);
+		});
+	}
 
-  refreshGroups(event: any) {
-    setTimeout(() => {
-      this.getGroups();
-      event.target.complete();
-    }, 2000);
-  }
+	refreshGroups(event: any) {
+		setTimeout(() => {
+			this.getGroups();
+			event.target.complete();
+		}, 2000);
+	}
 
-  async deleteGroup(group: Group) {
-    const alert = await this.alertController.create({
-      header: 'Leave Group',
-      message: 'Are you sure you want to leave this group?',
-      buttons: [
-        {
-          text: 'Yes',
-          handler: () => {
-            this.groupService.removeMember(group.id, this.authUser.uid).then((response) => {
-              if (group.createdBy == this.authUser.uid) {
-                this.groupService.assignNewCreator(group).then((response) => this.getGroups());
-              }
-            });
-          }
-        },
-        {
-          text: 'No'
-        }
-      ]
-    });
+	async deleteGroup(group: Group) {
+		const alert = await this.alertController.create({
+			header: 'Leave Group',
+			message: 'Are you sure you want to leave this group?',
+			buttons: [
+				{
+					text: 'Yes',
+					handler: () => {
+						this.groupService
+							.removeMember(group.id, this.authUser.uid)
+							.then(response => {
+								if (group.createdBy === this.authUser.uid) {
+									this.groupService
+										.assignNewCreator(group)
+										.then(() => this.getGroups());
+								}
+							});
+					}
+				},
+				{
+					text: 'No'
+				}
+			]
+		});
 
-    await alert.present();
-  }
+		await alert.present();
+	}
 
-  toggleSideNav() {
-    this.menuController.toggle();
-  }
+	toggleSideNav() {
+		this.menuController.toggle();
+	}
 
-  navigateToAdd() {
-    this.router.navigate(['groups/add']);
-  }
+	navigateToAdd() {
+		this.router.navigate(['groups/add']);
+	}
 
-  navigateToDetails(group: Group) {
-    this.dataService.changeData(group);
-    this.router.navigate(['groups/details']);
-  }
+	navigateToDetails(group: Group) {
+		this.dataService.changeData(group);
+		this.router.navigate(['groups/details']);
+	}
 
-  navigateToCalendar(group: Group) {
-    this.dataService.changeData(group);
-    this.router.navigate(['calendar']);
-  }
+	navigateToCalendar(group: Group) {
+		this.dataService.changeData(group);
+		this.router.navigate(['calendar']);
+	}
 }
