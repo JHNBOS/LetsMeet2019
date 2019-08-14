36cd8f52ff9b98df8c655391cbbcc2a120d9d4ef
Johan Bos <bosbosjohan@gmail.com>
2019-08-14 16:54:16 +0200

    Finished new design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Platform } from '@ionic/angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+	selector: 'app-root',
+	templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
-    this.initializeApp();
-  }
+	constructor(
+		private platform: Platform,
+		private splashScreen: SplashScreen,
+		private statusBar: StatusBar
+	) {
+		this.initializeApp();
+	}
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+	initializeApp() {
+		this.platform.ready().then(() => {
+			this.statusBar.styleBlackTranslucent();
+			this.splashScreen.hide();
+		});
+	}
 }
